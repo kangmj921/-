735b951606f6928f2500858bb9239a70cf629f4a
kangmj921 <kangmj921@gmail.com>
2021-04-21 13:27:04 +0900

    시간 초과 해결

--- 백준문제/DFS와 BFS/2206_벽 부수고 이동하기.py
+++ 백준문제/DFS와 BFS/2206_벽 부수고 이동하기.py
@@ -7,7 +7,10 @@
 # 시간초과가 난다. 더 경우의 수를 줄이든지 접근 방법을 바꿔야 할 것 같다..
 # 더 경우의 수를 줄이려면 주위에 최소 0이 2개는 되야 할 것 같다. 그래야 이동경로 상 막고 있는
 # 벽일 경우가 높다. 그래도 시간초과가 걸린다. 접근 방식을 바꿔야 할 것 같다.
-#
+# 탐색을 하는 동안 벽을 세울 수 있도록 해야할 것 같다.
+# 이제부터 최단거리는 visited 를 이용해서 구하면 될 것같다.
+# visited를 3차원 리스트로 구현해서 벽을 이미 세웠을 때랑 아닐 때랑 구분해서 기록하는 방식
+# 으로 해야 될 것 같다. 이 방법으로 하니까 해결 되었다.
 from collections import deque
 
 
@@ -15,40 +18,29 @@ N, M = map(int, input().split())
 maze_map = []
 for i in range(N):
     maze_map.append(list(map(int, input().rstrip('\n'))))
-wall_list = []
-record_wall = [[0] * M for i in range(N)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-wall_count = 0
 check_arrived = False
-min_distance = 1e9
-visited = [[0] * M for i in range(N)]
+visited = [[[0] * M for _ in range(N)] for _ in range(2)]
 queue = deque()
-queue.append((0, 0))
-visited[0][0] = 1
-per_distance = queue[-1]
-per_result = 0
+queue.append((0, 0, 0))
+visited[0][0][0] = 1
 while queue:
-    y, x = queue.popleft()
+    y, x, wall_count = queue.popleft()
     if (y, x) == (N-1, M-1):
         check_arrived = True
-        min_distance = min(min_distance, per_result + 1)
+        print(visited[wall_count][N-1][M-1])
+        exit()
     for i in range(4):
         ny = y + dy[i]
         nx = x + dx[i]
         if ny < 0 or ny >= N or nx < 0 or nx >= M:
             continue
-        if maze_map[ny][nx] == 1:
-            continue
-        if maze_map[ny][nx] == 0 and visited[ny][nx] == 0:
-            visited[ny][nx] = 1
-            queue.append((ny, nx))
-    if (y, x) == per_distance:
-        per_result += 1
-        if len(queue) != 0:
-            per_distance = queue[-1]
-    # print(queue, per_distance, per_result, min_distance, b_y, b_x)
-if check_arrived:
-    print(min_distance)
-else:
+        if maze_map[ny][nx] == 1 and wall_count == 0:
+            visited[wall_count + 1][ny][nx] = visited[wall_count][y][x] + 1
+            queue.append((ny, nx, 1))
+        if maze_map[ny][nx] == 0 and visited[wall_count][ny][nx] == 0:
+            visited[wall_count][ny][nx] = visited[wall_count][y][x] + 1
+            queue.append((ny, nx, wall_count))
+if not check_arrived:
     print(-1)
