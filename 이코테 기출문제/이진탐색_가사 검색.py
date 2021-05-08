cb644d130ed2a685756613698c015f6511bc7f5b
kangmj921 <62694182+kangmj921@users.noreply.github.com>
2021-05-08 11:30:39 +0900

    아직 시간초과 해결 못함

--- 이코테 기출문제/이진탐색_가사 검색.py
+++ 이코테 기출문제/이진탐색_가사 검색.py
@@ -2,6 +2,22 @@ array = [[] for _ in range(10001)]
 reversed_array = [[] for _ in range(10001)]
 
 
+def search(keyword, word):
+    print(keyword, word)
+    start = 0
+    end = len(word) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if keyword[mid] == '?':
+            end = mid - 1
+        else:
+            if keyword[start] == word[start]:
+                start += 1
+            else:
+                return 0
+    return 1
+
+
 def solution(words, queries):
     answer = []
     for word in words:
@@ -11,10 +27,17 @@ def solution(words, queries):
         array[i].sort()
         reversed_array[i].sort()
     for q in queries:
-        if q[0] != '?':
-            pass
+        result = 0
+        if q[0] != '?':     # 접미사에 ?가 있음.
+            for i in array[len(q)]:
+                result += search(q, i)
+            answer.append(result)
+        else:
+            for i in reversed_array[len(q)]:
+                result += search(q[::-1], i)
+            answer.append(result)
     return answer
 
 
-solution(["frodo", "front", "frost", "frozen", "frame", "kakao"],
-         ["fro??", "????o", "fr???", "fro???", "pro?"])
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"],
+         ["fro??", "????o", "fr???", "fro???", "pro?"]))
