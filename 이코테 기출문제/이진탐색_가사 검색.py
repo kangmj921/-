7f504e81d3e719ccd89ffcc257a784b9cbe0b187
kangmj921 <62694182+kangmj921@users.noreply.github.com>
2021-05-08 12:08:53 +0900

    시간초과 해결..

--- 이코테 기출문제/이진탐색_가사 검색.py
+++ 이코테 기출문제/이진탐색_가사 검색.py
@@ -1,23 +1,19 @@
 # 이진 탐색을 이용해서 구하려는데, 정확성 테스트는 통과를 하지만, 효율성 테스트에서
 # 문제가 있다. 탐색 부분을 좀 더 최적화를 시켜야할 것 같다.
+# 키워드에 따른 단어 부분을 탐색할 때, 반복문이 2중 구조로 되어있는 것이 주요
+# 원인인 것 같다. 그리고, 각 키워드에 대해서 맞는 단어들을 찾을 때,
+# 키워드의 ?를 a로 바꾼 것을 start, ?를 z로 바꾼 것을 end로 하면서
+# 각 단어가 그 사이에 있는 단어인지를 이분탐색으로 탐색하는 것이 훨씬 더 효율적이란
+# 것을 알 수 있었다.
+from bisect import bisect_right, bisect_left
 array = [[] for _ in range(10001)]
 reversed_array = [[] for _ in range(10001)]
 
 
-def search(keyword, word):
-    # print(keyword, word)
-    start = 0
-    end = len(word) - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if keyword[mid] == '?':
-            end = mid - 1
-        else:
-            if keyword[start] == word[start]:
-                start += 1
-            else:
-                return 0
-    return 1
+def search(array, start, end):
+    left = bisect_left(array, start)
+    right = bisect_right(array, end)
+    return right - left
 
 
 def solution(words, queries):
@@ -31,12 +27,10 @@ def solution(words, queries):
     for q in queries:
         result = 0
         if q[0] != '?':     # 접미사에 ?가 있음.
-            for i in array[len(q)]:
-                result += search(q, i)
+            result = search(array[len(q)], q.replace('?', 'a'), q.replace('?', 'z'))
             answer.append(result)
         else:
-            for i in reversed_array[len(q)]:
-                result += search(q[::-1], i)
+            result = search(reversed_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
             answer.append(result)
     return answer
 
