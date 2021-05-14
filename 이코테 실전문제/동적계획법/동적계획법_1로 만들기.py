b78c72235acda10f0185eda54d055f560b94b100
kangmj921 <62694182+kangmj921@users.noreply.github.com>
2021-05-14 13:05:38 +0900

    아직 해결 안함

---
+++ 이코테 실전문제/동적계획법/동적계획법_1로 만들기.py
@@ -0,0 +1,11 @@
+X = int(input())
+result = [0] * (X + 1)
+for i in range(2, X + 1):
+    result[i] = result[i - 1] + 1
+    if i % 2 == 0:
+        result[i] = min(result[i], result[i // 2] + 1)
+    if i % 3 == 0:
+        result[i] = min(result[i], result[i // 3] + 1)
+    if i % 5 == 0:
+        result[i] = min(result[i], result[i // 5] + 1)
+print(result[X])
