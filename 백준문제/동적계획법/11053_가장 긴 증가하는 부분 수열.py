72e02b38f6482630842ea9dea12866499b0b4411
kangmj921 <62694182+kangmj921@users.noreply.github.com>
2021-05-18 12:59:24 +0900

    first commit

--- 백준문제/동적계획법/11053_가장 긴 증가하는 부분 수열.py
+++ 백준문제/동적계획법/11053_가장 긴 증가하는 부분 수열.py
@@ -1,12 +1,17 @@
-from collections import deque
-
+# dp를 이용해서 풀 수 있는 핵심적인 유형 중 하나이다.
+# LIS 알고리즘이라고 따로 이름이 붙어있기도 하다.
+# 주어진 리스트에서 인덱스를 한 칸씩 늘려가면서
+# 내부 반복문으로 인덱스보다 작은 인덱스들을 살펴보면서 값이 인덱스의
+# 원소 값보다 작은 것이 있을 경우, 길이를 저장하는 리스트를 업데이트함.
+# 이 때 시간복잡도는 O(N^2)이지만, N의 값이 최대 1,000이라
+# 시간 제한에는 걸리지 않는다. N이 커지면 효율적으로 할 수 있도록
+# 하는 방법을 생각해봐야겠다.
 N = int(input())
 num_list = list(map(int, input().split()))
-result = deque()
-for i in range(N, 0, -1):
-    stack = []
-    if i == 0:
-        result.append([num_list[-1]])
-    else:
-        stack.append()
-print(result)
+length_list = [0] * N
+for i in range(N):
+    length_list[i] = 1
+    for j in range(i):
+        if num_list[j] < num_list[i]:
+            length_list[i] = max(length_list[i], 1 + length_list[j])
+print(max(length_list))
