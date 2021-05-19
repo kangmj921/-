3a8afacdcf1ee59264adef633223e0d82a88e4f9
kangmj921 <kangmj921@gmail.com>
2021-05-19 22:05:39 +0900

    first commit

--- 이코테 기출문제/동적계획법_퇴사.py
+++ 이코테 기출문제/동적계획법_퇴사.py
@@ -1,16 +1,22 @@
-# 점화식 생각이 잘 안난다.
+# 맨 뒤에서 부터 다이나믹 프로그래밍을 진행한다.
+# 진행되는 날짜가 선택되려면, 해당 일 수 가 넘어가지않도록 하는 기간을 가진
+# 상담 일정 중에 상담 기간이 가장 길거나, 금액이 가장 큰 것을 계속 고르면 된다.
 N = int(input())
 schedule_list = []
 for _ in range(N):
     schedule_list.append(list(map(int, input().split())))
-dp_list = [[0, 0] for _ in range(N)]
-dp_list[0] = schedule_list[0]
-for i in range(N):
-    j = i + schedule_list[i][0]
-    while j < N:
-        dp_list[j][1] = max(dp_list[j][1], dp_list[i][1] + schedule_list[j][1])
-        if dp_list[j][1] > dp_list[i][1] + schedule_list[j][1]:
-            j += dp_list[j][0]
-        else:
-            j += dp_list[i][0] + schedule_list[j][0]
-print(dp_list)
+dp_list = [0] * N
+for i in range(N - 1, -1, -1):
+    present_pay = schedule_list[i][1]
+    present_day = schedule_list[i][0]
+    if present_day > N - i:
+        if i != N - 1:
+            dp_list[i] = dp_list[i + 1]
+        continue
+    if i == N - 1:
+        dp_list[i] = present_pay
+    elif i + present_day == N:
+        dp_list[i] = max(present_pay, dp_list[i + 1])
+    else:
+        dp_list[i] = max(present_pay + dp_list[i + present_day], dp_list[i + 1])
+print(max(dp_list))
