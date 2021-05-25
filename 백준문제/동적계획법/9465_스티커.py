f508350cabb08482e1d46bafdc3e2a2b5e6c31ac
kangmj921 <62694182+kangmj921@users.noreply.github.com>
2021-05-25 13:54:06 +0900

    first commit

--- 백준문제/동적계획법/9465_스티커.py
+++ 백준문제/동적계획법/9465_스티커.py
@@ -1,11 +1,16 @@
-# 현재의 스티커가 선택되기 위해서는, 현재의 스티커 점수가
-# 훼손되는 스티커 점수보다 클 경우이거나, 아니면 지금까지 선택된
-# 스티커들이 선택할 스티커와 한 변을 공유하지 않는 경우이다.
-# 점화식은 좀 더 고민해야될 듯.
+# dp_list[i][j]는 i열의 j 행 스티커를 선택하였을 때 얻을 수 있는 최대 점수를 저장한다.
+# 현재의 스티커가 선택되기 위해서는, 대각선에 위치한 스티커를 선택하였거나
+# 한 변을 공유하고 있지 않은 스티커를 선택하였을 경우이다.
+# 이 중 최대 점수를 얻을 수 있는 값을 선택하고 현재의 스티커 값을 더해서 저장한다.
 for _ in range(int(input())):
     n = int(input())
     sticker_list = [list(map(int, input().split())), list(map(int, input().split()))]
     dp_list = [[0] * n for _ in range(2)]
-    for i in range(2):
-        for j in range(n):
-            dp_list[i][j] = max()
+    dp_list[0][0] = sticker_list[0][0]
+    dp_list[0][1] = sticker_list[0][1] + sticker_list[1][0]
+    dp_list[1][0] = sticker_list[1][0]
+    dp_list[1][1] = sticker_list[1][1] + sticker_list[0][0]
+    for i in range(2, n):
+        dp_list[0][i] = sticker_list[0][i] + max(dp_list[1][i - 1], dp_list[1][i - 2])
+        dp_list[1][i] = sticker_list[1][i] + max(dp_list[0][i - 1], dp_list[0][i - 2])
+    print(max(dp_list[0][n-1], dp_list[1][n-1]))
