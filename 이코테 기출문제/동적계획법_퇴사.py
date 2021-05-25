42da4f381de3398f408b522227a757e104684a96
kangmj921 <62694182+kangmj921@users.noreply.github.com>
2021-05-25 11:50:36 +0900

    문제 해결 과정 보충

--- 이코테 기출문제/동적계획법_퇴사.py
+++ 이코테 기출문제/동적계획법_퇴사.py
@@ -1,6 +1,10 @@
 # 맨 뒤에서 부터 다이나믹 프로그래밍을 진행한다.
-# 진행되는 날짜가 선택되려면, 해당 일 수 가 넘어가지않도록 하는 기간을 가진
-# 상담 일정 중에 상담 기간이 가장 길거나, 금액이 가장 큰 것을 계속 고르면 된다.
+# 뒤쪽 날짜부터의 상담에 대해여 현재 상담일자의 이윤(present_pay) +
+# 현재 상담을 마친 일자부터의 최대이윤(dp_list[i + present_day])을 계산하고
+# 이후에 계산된 각각의 값 중 최댓값을 저장하면 된다.
+# dp_list[i] = i 번째 날 부터 마지막 날까지 낼 수 있는 최대 이익이라고 하면,
+# 점화식은 dp_list[i] = max(present_pay + dp_list[i + present_day], max_value)이다.
+# 이 때, max_value는 현재까지의 최대 상담 금액에 해당하는 것이므로, dp_list[i + 1]과 같다.
 N = int(input())
 schedule_list = []
 for _ in range(N):
