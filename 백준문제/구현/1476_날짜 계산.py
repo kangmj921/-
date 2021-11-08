013cda97f1b506e637fa84329ddb3645a54855d3
kangmj921 <kangmj921@gmail.com>
2021-11-08 23:13:04 +0900

    아직 해결 못함

---
+++ 백준문제/구현/1476_날짜 계산.py
@@ -0,0 +1,6 @@
+# 주어진 E, S, M 값은 각각 15, 28, 19에 대한 나머지 값들이다.
+# 15, 28, 19가 서로 같은 결과를 가질때 몫 값이 가장 빠른 연도이다.
+# result = 15 * Q + E, 28 * Q +
+E, S, M = map(int, input().split())
+while E != M or S != M or E != S:
+    pass
