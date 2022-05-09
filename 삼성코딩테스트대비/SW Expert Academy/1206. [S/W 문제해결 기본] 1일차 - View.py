3e1c10bdca8de3968c222607db833c5dc1d783bc
kangmj921 <kangmj921@gmail.com>
2022-05-10 02:37:47 +0900

    first commit

--- 삼성코딩테스트대비/SW Expert Academy/1206. [S/W 문제해결 기본] 1일차 - View.py
+++
@@ -1,9 +0,0 @@
-for T in range(10):
-    apart_number = int(input())
-    apart_length = list(map(int, input().split()))
-    answer, temp = 0, 0
-    for i in range(2, apart_number - 2):
-        temp = apart_length[i] - max(apart_length[i - 1], apart_length[i - 2], apart_length[i + 1], apart_length[i + 2])
-        if temp > 0:
-            answer += temp
-    print("#%d %d" % (T + 1, answer))
