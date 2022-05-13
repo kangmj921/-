b5fc21238896bdcdbfc80cc49f5211243f15b897
kangmj921 <kangmj921@gmail.com>
2022-05-13 18:35:24 +0900

    first commit

---
+++ 삼성코딩테스트대비/SW Expert Academy/1208. [SW 문제해결 기본] 1일차 - Flatten.py
@@ -0,0 +1,15 @@
+# 그리디한 방식으로 높이 별로 정렬한다음 가장 높은 박스에서 낮은 박스의 높이를 1개씩 dump하고
+# 다시 정렬하는 방식으로 하였다.
+# dump 횟수가 최대 1000이고 입력 받는 박스 줄의 개수가 100이므로
+# 최대 O(100)*1000이기 때문에 그리디한 방식으로 하였다.
+# 만약 입력 받는 박스의 줄의 개수나 dump 횟수가 더 많다면 다른 방식을 써야할 것이다.
+#
+for T in range(10):
+    dump_num = int(input())
+    height_list = sorted(list(map(int, input().split())))
+    while dump_num > 0:
+        height_list[-1] -= 1
+        height_list[0] += 1
+        dump_num -= 1
+        height_list = sorted(height_list)  # 한 번 정렬하는데 O(N)임
+    print("#{} {}".format(T + 1, height_list[-1] - height_list[0]))
