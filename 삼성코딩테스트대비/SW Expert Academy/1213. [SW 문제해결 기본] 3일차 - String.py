52b2402be1f445a98f1646c24c748fdd87c0192c
kangmj921 <kangmj921@gmail.com>
2022-05-17 11:58:22 +0900

    first commit

---
+++ 삼성코딩테스트대비/SW Expert Academy/1213. [SW 문제해결 기본] 3일차 - String.py
@@ -0,0 +1,23 @@
+def solution(v, n):
+    global answer
+    temp = []
+    if n >= len(target_string):
+        answer = len(v)
+        return
+    for i in range(len(v)):
+        if v[i] + 1 < len(input_string) and input_string[v[i] + 1] == target_string[n]:
+            temp.append(v[i] + 1)
+    solution(temp, n + 1)
+    return
+
+
+for _ in range(10):
+    T = int(input())
+    target_string = input().rstrip('\n')
+    input_string = input().rstrip('\n')
+    visited, answer = [], 0
+    for i in range(len(input_string)):
+        if input_string[i] == target_string[0]:
+            visited.append(i)
+    solution(visited, 1)
+    print("#{} {}".format(T, answer))
