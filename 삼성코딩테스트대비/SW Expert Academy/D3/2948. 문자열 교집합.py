a899b6e7d4d641a63ec2c07f8902763dc7f2fec8
kangmj921 <kangmj921@gmail.com>
2022-05-25 12:36:35 +0900

    first commit

--- 삼성코딩테스트대비/SW Expert Academy/D3/2948. 문자열 교집합.py
+++ 삼성코딩테스트대비/SW Expert Academy/D3/2948. 문자열 교집합.py
@@ -1,3 +1,6 @@
+# set 자료형을 쓰면 간단해지는 문제이지만, set을 활용하지 못하는 조건이
+# 주어진다면 set 자료형을 직접 구현해야(hast 알고리즘) 시간초과에 걸리지 않을것
+# hash 알고리즘에 대해 알아야함.
 for T in range(int(input())):
     N, M = map(int, input().split())
     n_list = list(input().split())
