78177374421f2402d53ded0dcf6011ead5697fb9
kangmj921 <kangmj921@gmail.com>
2022-06-22 19:08:54 +0900

    first commit

--- 백준문제/구현/1966_프린터 큐.py
+++ 백준문제/구현/1966_프린터 큐.py
@@ -2,6 +2,28 @@
 # queue의 가장 앞의 문서를 인쇄(pop)해야 하므로, 리스트의 맨 앞의 원소를 삭제하는데 O(N)의 시간이 걸린다.
 #
 #
+import heapq
+from collections import deque
+
+
 for _ in range(int(input())):
     num_of_documents, target_document = map(int, input().split())
-    document_list = list(map(int, input().split()))
+    input_list = deque(map(int, input().split()))
+    document_list = deque()
+    que = []
+    i = 0
+    for d in input_list:
+        heapq.heappush(que, (-1 * d))
+        document_list.append([d, i])
+        i += 1
+    answer = 0
+    i = 0
+    while document_list:
+        if -1 * que[0] > document_list[i][0]:
+            document_list.append(document_list.popleft())
+        else:
+            heapq.heappop(que)
+            c = document_list.popleft()
+            answer += 1
+            if c[1] == target_document:
+                print(answer)
