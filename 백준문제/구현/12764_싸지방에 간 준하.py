f138598ec514022615114edebfc8da014acdfbf6
kangmj921 <kangmj921@gmail.com>
2022-07-02 21:47:05 +0900

    python3 시간초과 해결

--- 백준문제/구현/12764_싸지방에 간 준하.py
+++ 백준문제/구현/12764_싸지방에 간 준하.py
@@ -9,32 +9,47 @@
 # 아니라면, 해당 index에 해당하는 컴퓨터를 쓸 수 있으므로, 해당 index에 해당하는 count의 원소 + 1
 
 # python3에서는 계속 시간초과가 나지만, pypy3에선 안난다.
-# 다른 탐색 방법을 사용해야될 것 같다.
-
+# 비는 컴퓨터 자리를 찾는 탐색 방법을 미리 자리를 저장해 놓는 방식으로 바꿔야 할 것 같다.
+# 먼저 시작 시간에 따라 사람의 컴퓨터 이용 시간을 정렬한 뒤, 먼저 온 사람대로 젤 왼쪽의 컴퓨터를
+# 배정하는데, 우선순위 큐에 배정된 사람의 종료시간과 배정받은 컴퓨터 번호를 저장하는 리스트를 넣는다.
+# 이러면, 우선순위 큐에는 젤 먼저 종료하는 사람의 종료 시간과 컴퓨터 번호가 젤 위에 오게 될 것이다.
+# 
+#
 import heapq
 import sys
 
 
 N = int(input())
 priority_que = []
-computer_list = [0] * N
-count = [0] * N
+computer_list = [i for i in range(1, N + 1)]
+time_list = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
+time_list.sort(key=lambda x: x[0])
+count = [0] * (N + 1)
 answer = 0
-stack = []
-for _ in range(N):
-    P, Q = map(int, sys.stdin.readline().split())
-    heapq.heappush(priority_que, [P, Q])
-while priority_que:
-    temp = heapq.heappop(priority_que)
-    check = False
-    for i in range(len(computer_list)):
-        if computer_list[i] <= temp[0]:
-            if not computer_list[i]:
-                answer += 1
-            computer_list[i] = temp[1]
-            count[i] += 1
-            break
+for P, Q in time_list:
+    while priority_que and priority_que[0][0] < P:
+        temp = heapq.heappop(priority_que)
+        heapq.heappush(computer_list, temp[1])
+    nxt = heapq.heappop(computer_list)
+    heapq.heappush(priority_que, (Q, nxt))
+    count[nxt] += 1
+    if len(priority_que) > answer:
+        answer = len(priority_que)
 print(answer)
 for i in count:
     if i:
         print(i, end=" ")
+
+# 기존에 시간초과 되었던 풀이
+# for _ in range(N):
+#     P, Q = map(int, sys.stdin.readline().split())
+#     heapq.heappush(priority_que, [P, Q])
+# while priority_que:
+#     temp = heapq.heappop(priority_que)
+#     for i in range(len(computer_list)):
+#         if computer_list[i] <= temp[0]:
+#             if not computer_list[i]:
+#                 answer += 1
+#             computer_list[i] = temp[1]
+#             count[i] += 1
+#             break
